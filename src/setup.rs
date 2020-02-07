788ab2dc0addc19713063f661090c5a023e3c29b
Benoit Senechal <benoit.senechal@telecom-paris.fr>
2020-02-07 10:47:05 +0100

    adapted setup.rs to drone-OS

--- src/setup.rs
+++ src/setup.rs
@@ -1,70 +1,121 @@
-use stm32f4xx_hal::stm32::{RCC, SYSCFG};
-#[cfg(feature = "nucleo-f429zi")]
-use stm32f4xx_hal::gpio::{
-    gpioa::{PA1, PA2, PA7},
-    gpiob::{PB13},
-    gpioc::{PC1, PC4, PC5},
-    gpiog::{PG11, PG13},
-    Speed::VeryHigh,
+use drone_cortex_m::{reg::prelude::*};
+
+use drone_stm32_map::{
+    periph::{
+        gpio::{GpioPortPeriph, GpioA, GpioB, GpioC, GpioG},
+    },
+    reg,
 };
 
 /// Initialize GPIO pins. Enable syscfg and ethernet clocks. Reset the
 /// Ethernet MAC.
 ///
 /// If supported, you should also call `setup_pins()`.
-pub fn setup(rcc: &RCC, syscfg: &SYSCFG) {
+pub fn setup(
+    rcc_apb2enr: reg::rcc::Apb2Enr<Srt>,
+    rcc_ahb1enr: reg::rcc::Ahb1Enr<Srt>,
+    rcc_ahb1rstr: reg::rcc::Ahb1Rstr<Srt>, 
+    syscfg_pmc: reg::syscfg::Pmc<Srt>,
+    ) {
     // enable syscfg clock
-    rcc.apb2enr.modify(|_, w| w.syscfgen().set_bit());
+    rcc_apb2enr.modify(|r| r.set_syscfgen());
 
     // select MII or RMII mode
     // 0 = MII, 1 = RMII
-    syscfg.pmc.modify(|_, w| w.mii_rmii_sel().set_bit());
+    syscfg_pmc.modify(|r| r.set_mii_rmii_sel());
 
     // enable ethernet clocks
-    rcc.ahb1enr.modify(|_, w| {
-        w.ethmacen()  .set_bit()
-         .ethmactxen().set_bit()
-         .ethmacrxen().set_bit()
+    rcc_ahb1enr.modify(|r| {
+        r.set_ethmacen()
+         .set_ethmactxen()
+         .set_ethmacrxen()
     });
 
-    reset_pulse(&rcc);
+    reset_pulse(rcc_ahb1rstr);
 }
 
-fn reset_pulse(rcc: &RCC) {
-    rcc.ahb1rstr.modify(|_, w| w.ethmacrst().set_bit());
-    rcc.ahb1rstr.modify(|_, w| w.ethmacrst().clear_bit());
+fn reset_pulse(
+    rcc_ahb1rstr: reg::rcc::Ahb1Rstr<Srt>,
+    ) {
+    rcc_ahb1rstr.modify(|r| r.set_ethmacrst());
+    rcc_ahb1rstr.modify(|r| r.clear_ethmacrst());
 }
 
-/// Pin setup for the **STM32 Nucleo-F429ZI** dev board
-/// (feature: `nucleo-f429zi`)
+/// Pin setup for the **OLIMEX STM32-E407** dev board
 ///
 /// Set RMII pins to
 /// * Alternate function 11
 /// * High-speed
-///
-/// This function consumes the pins so that you cannot use them
-/// anywhere else by accident.
-#[cfg(feature = "nucleo-f429zi")]
-pub fn setup_pins<M1, M2, M3, M4, M5, M6, M7, M8, M9>(
-    pa1: PA1<M1>, pa2: PA2<M2>, pa7: PA7<M3>, pb13: PB13<M4>, pc1: PC1<M5>,
-    pc4: PC4<M6>, pc5: PC5<M7>, pg11: PG11<M8>, pg13: PG13<M9>
+pub fn setup_pins(
+    gpio_a: &mut GpioPortPeriph<GpioA>,
+    gpio_b: &mut GpioPortPeriph<GpioB>,
+    gpio_c: &mut GpioPortPeriph<GpioC>,
+    gpio_g: &mut GpioPortPeriph<GpioG>
 ) {
-    // PA1 RMII Reference Clock - SB13 ON
-    pa1.into_alternate_af11().set_speed(VeryHigh);
-    // PA2 RMII MDIO - SB160 ON
-    pa2.into_alternate_af11().set_speed(VeryHigh);
-    // PC1 RMII MDC - SB164 ON
-    pc1.into_alternate_af11().set_speed(VeryHigh);
-    // PA7 RMII RX Data Valid D11 JP6 ON
-    pa7.into_alternate_af11().set_speed(VeryHigh);
-    // PC4 RMII RXD0 - SB178 ON
-    pc4.into_alternate_af11().set_speed(VeryHigh);
-    // PC5 RMII RXD1 - SB181 ON
-    pc5.into_alternate_af11().set_speed(VeryHigh);
-    // PG11 RMII TX Enable - SB183 ON
-    pg11.into_alternate_af11().set_speed(VeryHigh);
-    // PG13 RXII TXD0 - SB182 ON
-    pg13.into_alternate_af11().set_speed(VeryHigh);
-    // PB13 RMII TXD1 I2S_A_CK JP7 ON
-    pb13.into_alternate_af11().set_speed(VeryHigh);
+    // GPIOA 1 - 2 - 7
+    // speed to Very high
+    // AF 11
+    gpio_a.gpio_ospeedr.modify(|r| {
+        r.write_ospeedr1(0b11)
+            .write_ospeedr2(0b11)
+            .write_ospeedr7(0b11)
+    });
+    gpio_a.gpio_moder.modify(|r| {
+        r.write_moder1(0b10)
+            .write_moder2(0b10)
+            .write_moder7(0b10)
+    });  
+    gpio_a.gpio_afrl.modify(|r| {
+        r.write_afrl1(11)
+            .write_afrl2(11)
+            .write_afrl7(11)
+    });
+
+    // GPIOB 13
+    // speed to Very high
+    // AF 11
+    gpio_b.gpio_ospeedr.modify(|r| {
+        r.write_ospeedr13(0b11)
+    });
+    gpio_b.gpio_moder.modify(|r| {
+        r.write_moder13(0b10)
+    });  
+    gpio_b.gpio_afrh.modify(|r| {
+        r.write_afrh13(11)
+    });
+
+    // GPIOC 1 - 4 - 5
+    // speed to Very high
+    // AF 11
+    gpio_c.gpio_ospeedr.modify(|r| {
+        r.write_ospeedr1(0b11)
+            .write_ospeedr4(0b11)
+            .write_ospeedr5(0b11)
+    });
+    gpio_c.gpio_moder.modify(|r| {
+        r.write_moder1(0b10)
+            .write_moder4(0b10)
+            .write_moder5(0b10)
+    });  
+    gpio_c.gpio_afrl.modify(|r| {
+        r.write_afrl1(11)
+            .write_afrl4(11)
+            .write_afrl5(11)
+    });
+
+    // GPIOG 11 - 13
+    // speed to Very high
+    // AF 11
+    gpio_g.gpio_ospeedr.modify(|r| {
+        r.write_ospeedr11(0b11)
+            .write_ospeedr13(0b11)
+    });
+    gpio_g.gpio_moder.modify(|r| {
+        r.write_moder11(0b10)
+            .write_moder13(0b10)
+    });  
+    gpio_g.gpio_afrh.modify(|r| {
+        r.write_afrh11(11)
+            .write_afrh13(11)
+    });
 }
